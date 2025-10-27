c4631d0dab9c1752957132ac7b16af9865708b69
Oleksandr Bob <oleksandr.bob156@gmail.com>
2025-10-27 13:17:12 +0200

    updating flow

--- users-service/main.go
+++ users-service/main.go
@@ -3,15 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
-	"github.com/OleksandrBob/nextseasonlist/users-service/db"
-	"github.com/OleksandrBob/nextseasonlist/users-service/db/migrations"
-	"github.com/OleksandrBob/nextseasonlist/users-service/handlers"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-
-	sharedMiddlewares "github.com/OleksandrBob/nextseasonlist/shared/middlewares"
 )
 
 func main() {
@@ -23,48 +19,50 @@ func main() {
 	mongoURI := os.Getenv("MONGO_URI")
 	if mongoURI == "" {
 		fmt.Println("mongo uri is unset")
-		return
+		//return
 	}
 
-	err = db.ConnectDb(mongoURI)
-	defer db.DisconnectDb()
-	if err != nil {
-		return
+	// err = db.ConnectDb(mongoURI)
+	// defer db.DisconnectDb()
+	// if err != nil {
+	// 	return
+	// }
+
+	// if err = migrations.Migrate_v1(); err != nil {
+	// 	log.Println(err.Error())
+	// 	return
+	// }
+
+	// userCollection := db.GetCollection(db.UsersCollection)
+	// tokenBlacklistCollection := db.GetCollection(db.BlacklistedTokensCollection)
+
+	// profileHandler := handlers.NewProfileHandler(userCollection)
+	// authHandler := handlers.NewAuthHandler(userCollection, tokenBlacklistCollection)
+	hand := func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello Bob"})
 	}
-
-	if err = migrations.Migrate_v1(); err != nil {
-		log.Println(err.Error())
-		return
-	}
-
-	userCollection := db.GetCollection(db.UsersCollection)
-	tokenBlacklistCollection := db.GetCollection(db.BlacklistedTokensCollection)
-
-	profileHandler := handlers.NewProfileHandler(userCollection)
-	authHandler := handlers.NewAuthHandler(userCollection, tokenBlacklistCollection)
 
 	router := gin.Default()
+	router.GET("/", hand)
 
-	authRoutes := router.Group("/auth")
-	{
-		authRoutes.POST("/login", authHandler.LoginUser)
-		authRoutes.POST("/logout", authHandler.LogOut)
-		authRoutes.POST("/register", authHandler.RegisterUser)
-		authRoutes.POST("/refreshToken", authHandler.RefreshToken)
-	}
+	// authRoutes := router.Group("/auth")
+	// {
+	// 	authRoutes.POST("/login", authHandler.LoginUser)
+	// 	authRoutes.POST("/logout", authHandler.LogOut)
+	// 	authRoutes.POST("/register", authHandler.RegisterUser)
+	// 	authRoutes.POST("/refreshToken", authHandler.RefreshToken)
+	// }
 
-	profileRoutes := router.Group("/profile", sharedMiddlewares.AuthMiddleware([]byte(os.Getenv("ACCESS_TOKEN_SECRET"))))
-	{
-		profileRoutes.GET("/", profileHandler.GetUserData)
-		profileRoutes.PUT("/", profileHandler.UpdatePersonalInfo)
-	}
+	// profileRoutes := router.Group("/profile", sharedMiddlewares.AuthMiddleware([]byte(os.Getenv("ACCESS_TOKEN_SECRET"))))
+	// {
+	// 	profileRoutes.GET("/", profileHandler.GetUserData)
+	// 	profileRoutes.PUT("/", profileHandler.UpdatePersonalInfo)
+	// }
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
-	// go jobs.StartJobs(tokenBlacklistCollection)
 
 	log.Println("Users-Server running on port: ", port)
 	router.Run(":" + port)
