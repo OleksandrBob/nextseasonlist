9dffb5daa5debf1cd545aeee4ca5f035946778a7
Oleksandr Bob <oleksandr.bob156@gmail.com>
2025-11-16 16:39:23 +0200

    [Added mongo url]

--- users-service/main.go
+++ users-service/main.go
@@ -10,8 +10,13 @@ import (
 	"math/rand"
 	"time"
 
+	"github.com/OleksandrBob/nextseasonlist/users-service/db"
+	"github.com/OleksandrBob/nextseasonlist/users-service/db/migrations"
+	"github.com/OleksandrBob/nextseasonlist/users-service/handlers"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+
+	sharedMiddlewares "github.com/OleksandrBob/nextseasonlist/shared/middlewares"
 )
 
 func main() {
@@ -27,22 +32,23 @@ func main() {
 	}
 	fmt.Println("Mongo URI:", mongoURI)
 
-	// err = db.ConnectDb(mongoURI)
-	// defer db.DisconnectDb()
-	// if err != nil {
-	// 	return
-	// }
+	err = db.ConnectDb(mongoURI)
+	defer db.DisconnectDb()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-	// if err = migrations.Migrate_v1(); err != nil {
-	// 	log.Println(err.Error())
-	// 	return
-	// }
+	if err = migrations.Migrate_v1(); err != nil {
+		log.Println(err.Error())
+		return
+	}
 
-	// userCollection := db.GetCollection(db.UsersCollection)
-	// tokenBlacklistCollection := db.GetCollection(db.BlacklistedTokensCollection)
+	userCollection := db.GetCollection(db.UsersCollection)
+	tokenBlacklistCollection := db.GetCollection(db.BlacklistedTokensCollection)
 
-	// profileHandler := handlers.NewProfileHandler(userCollection)
-	// authHandler := handlers.NewAuthHandler(userCollection, tokenBlacklistCollection)
+	profileHandler := handlers.NewProfileHandler(userCollection)
+	authHandler := handlers.NewAuthHandler(userCollection, tokenBlacklistCollection)
 
 	rand.Seed(time.Now().UnixNano())
 	randomNumber := rand.Intn(1000000)
@@ -87,19 +93,19 @@ func main() {
 		})
 	})
 
-	// authRoutes := router.Group("/auth")
-	// {
-	// 	authRoutes.POST("/login", authHandler.LoginUser)
-	// 	authRoutes.POST("/logout", authHandler.LogOut)
-	// 	authRoutes.POST("/register", authHandler.RegisterUser)
-	// 	authRoutes.POST("/refreshToken", authHandler.RefreshToken)
-	// }
+	authRoutes := router.Group("/auth")
+	{
+		authRoutes.POST("/login", authHandler.LoginUser)
+		authRoutes.POST("/logout", authHandler.LogOut)
+		authRoutes.POST("/register", authHandler.RegisterUser)
+		authRoutes.POST("/refreshToken", authHandler.RefreshToken)
+	}
 
-	// profileRoutes := router.Group("/profile", sharedMiddlewares.AuthMiddleware([]byte(os.Getenv("ACCESS_TOKEN_SECRET"))))
-	// {
-	// 	profileRoutes.GET("/", profileHandler.GetUserData)
-	// 	profileRoutes.PUT("/", profileHandler.UpdatePersonalInfo)
-	// }
+	profileRoutes := router.Group("/profile", sharedMiddlewares.AuthMiddleware([]byte(os.Getenv("ACCESS_TOKEN_SECRET"))))
+	{
+		profileRoutes.GET("/", profileHandler.GetUserData)
+		profileRoutes.PUT("/", profileHandler.UpdatePersonalInfo)
+	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
